0170544626ef11635f3ddfc72893a21a702a0542
Isaac-DeFrain <isaacdefrain@gmail.com>
2020-10-07 20:34:37 -0600

    A little refactor

--- risc.ml
+++ risc.ml
@@ -19,14 +19,18 @@ type risc_instruction =
   
 let string_of_reg (R i : register) = "R" ^ string_of_int i
 
+let string_of_regs r1 r2 r3 =
+  string_of_reg r1 ^ " " ^ string_of_reg r2 ^ " " ^ string_of_reg r3
+
 let print_risc = function
-  | LOADI (r, j) -> print_endline ("LOADI " ^ string_of_reg r ^ " #" ^ string_of_int j)
+  | LOADI (r, j) ->
+    print_endline ("LOADI " ^ string_of_reg r ^ " #" ^ string_of_int j)
   | LOAD  (r, s) -> print_endline ("LOAD "  ^ string_of_reg r ^ " " ^ s)
   | STORE (s, r) -> print_endline ("STORE "  ^ s ^ " " ^ string_of_reg r)
-  | ADD (r1, r2, r3) -> print_endline ("ADD " ^ string_of_reg r1 ^ " " ^ string_of_reg r2 ^ " " ^ string_of_reg r3)
-  | SUB (r1, r2, r3) -> print_endline ("SUB " ^ string_of_reg r1 ^ " " ^ string_of_reg r2 ^ " " ^ string_of_reg r3)
-  | MUL (r1, r2, r3) -> print_endline ("MUL " ^ string_of_reg r1 ^ " " ^ string_of_reg r2 ^ " " ^ string_of_reg r3)
-  | AND (r1, r2, r3) -> print_endline ("AND " ^ string_of_reg r1 ^ " " ^ string_of_reg r2 ^ " " ^ string_of_reg r3)
-  | XOR (r1, r2, r3) -> print_endline ("XOR " ^ string_of_reg r1 ^ " " ^ string_of_reg r2 ^ " " ^ string_of_reg r3)
+  | ADD (r1, r2, r3) -> print_endline ("ADD " ^ string_of_regs r1 r2 r3)
+  | SUB (r1, r2, r3) -> print_endline ("SUB " ^ string_of_regs r1 r2 r3)
+  | MUL (r1, r2, r3) -> print_endline ("MUL " ^ string_of_regs r1 r2 r3)
+  | AND (r1, r2, r3) -> print_endline ("AND " ^ string_of_regs r1 r2 r3)
+  | XOR (r1, r2, r3) -> print_endline ("XOR " ^ string_of_regs r1 r2 r3)
   | READ s -> print_endline ("READ " ^ s)
   | WRITE s -> print_endline ("WRITE " ^ s)
